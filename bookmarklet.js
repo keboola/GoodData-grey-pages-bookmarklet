d7c5a12237547236390687d3026cf30dea334a7e
Martin Halamicek <martin.halamicek@keboola.com>
2012-04-05 16:30:13 +0200

    fixed link to github repository

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,3 +1,3 @@
 var script = document.createElement('script');
-	script.src = 'https://github.com/keboola/GoodData-grey-pages-bookmarklet/blob/master/greypages.min.js?rand=' + Math.random();
+	script.src = 'https://raw.github.com/keboola/GoodData-grey-pages-bookmarklet/master/greypages.min.js?rand=' + Math.random();
 document.getElementsByTagName('head')[0].appendChild(script);
