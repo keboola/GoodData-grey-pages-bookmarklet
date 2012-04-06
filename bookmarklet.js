81ca655b7778e6f6442a35d5859a69d89c506355
Martin Halamicek <martin.halamicek@keboola.com>
2012-04-06 10:12:05 +0200

    fixed bookmarklet loading for Firefox

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,3 +1,5 @@
-var script = document.createElement('script');
-	script.src = 'https://raw.github.com/keboola/GoodData-grey-pages-bookmarklet/master/greypages.min.js?rand=' + Math.random();
-document.getElementsByTagName('head')[0].appendChild(script);
+(function() {
+	var script = document.createElement('script');
+		script.src = 'https://raw.github.com/keboola/GoodData-grey-pages-bookmarklet/master/greypages.min.js?rand=' + Math.random();
+	document.getElementsByTagName('head')[0].appendChild(script);
+}());
